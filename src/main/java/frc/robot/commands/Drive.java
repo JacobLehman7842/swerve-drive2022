6f094277a19e737f1222d368972d116db733eff3
Jacob Lehman <lehman.jacob7842@gmail.com>
2022-12-03 10:38:36 -0800

    added joystick to drive.java

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -6,22 +6,25 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class Drive extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final DriveTrain m_drivetrain;
-
+  private final Joystick m_joystick;
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public Drive(DriveTrain subsystem) {
-    m_drivetrain = subsystem;
+  public Drive(DriveTrain dt, Joystick j) {
+    m_drivetrain = dt;
+    m_joystick = j;
+
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(dt);
   }
 
   // Called when the command is initially scheduled.
